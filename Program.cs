70c38c3e2bb983be63a24b794dd83699909cc05a
Anton Bombin <step4321@mail.ru>
2024-12-05 20:11:08 +0300

    Составлен код для выполнения задачи

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿Console.WriteLine("Введите элементы массива через пробел: ");
+string input = Console.ReadLine();
+string[] initialArray = input.Split(' ');
+
+int count = 0;
+for (int i = 0; i < initialArray.Length; i++)
+{
+    if (initialArray[i].Length <= 3) count++;
+}
+
+string[] resultArray = new string[count];
+
+int index = 0;
+for (int i = 0; i < initialArray.Length; i++)
+{
+    if (initialArray[i].Length <= 3) 
+    {
+        resultArray[index] = initialArray[i];
+        index++;
+    }
+}
+
+Console.WriteLine("Результат: ");
+Console.WriteLine(string.Join(", ", resultArray));
+
